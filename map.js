5db428cf72b99b38ace7d5692c6028c0a1e0505a
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2020-02-02 04:22:30 -0300

    Gera mapa com loops

--- map.js
+++ map.js
@@ -1,41 +1,29 @@
 import Grass from "./src/components/map/tiles/Grass";
 import Poison from "./src/components/map/fields/Poison";
 
-export default [
-    {
-        x: 0,
-        y: 0,
-        stack: [
-            Grass,
-            Poison,
-        ]
-    },
-    {
-        x: 1,
-        y: 0,
-        stack: [
-            Grass,
-        ]
-    },
-    {
-        x: 2,
-        y: 0,
-        stack: [
-            Grass,
-        ]
-    },
-    {
-        x: 3,
-        y: 0,
-        stack: [
-            Grass,
-        ]
-    },
-    {
-        x: 4,
-        y: 0,
-        stack: [
-            Grass,
-        ]
-    },
-];
+const minX = 0;
+const minY = 0;
+const maxX = 15;
+const maxY = 15;
+
+const finalMap = [];
+
+for (let x = minX; x < maxX; x++) {
+    for (let y = minY; y < maxY; y++) {
+        const stack = [
+            Grass
+        ];
+
+        if (x === 2 && y === 3) {
+            stack.push(Poison);    
+        }
+
+        finalMap.push({
+            x,
+            y,
+            stack,
+        });
+    }
+}
+
+export default finalMap;
