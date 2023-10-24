7c56f1818d983d31fce7b0ef44721d7f1ebcb1af
Henrique <rikezenho@gmail.com>
2023-10-24 09:34:12 -0300

    Adjust animation when shift + key is pressed

--- src/components/alive/Player/index.js
+++ src/components/alive/Player/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import NameBar from "../../hud/NameBar";
 import VisualElement from "./visual";
-import baseProps from "../base";
 import {
   shouldNotCompleteMove,
   getFutureSqmInfo,
@@ -11,7 +10,6 @@ import { setPlayerPos } from "../../../store/actions/player";
 import { setMessage } from "../../../store/actions/hud";
 import { store } from "../../../store";
 import { log } from "../../../functions/gameUtils";
-import useKeyPress from "../../../hooks/useKeyPress";
 
 const calculateSpeed = (speed) => (1 / (speed / 100)) * 1;
 
@@ -48,35 +46,27 @@ const useWalkWithKeyboard = () => {
       let newPos = {};
 
       const keyPressed = `${event.key}`.toLowerCase();
-      const isShiftPressed = event.shiftKey;
+      const isShiftKeyPressed = event.shiftKey;
 
       if (keyPressed === "w") {
         newDirection = "up";
-        newPos = isShiftPressed
-          ? { x: pos.x, y: pos.y }
-          : { x: pos.x, y: pos.y - 1 };
+        newPos = { x: pos.x, y: pos.y - 1 };
       } else if (keyPressed === "s") {
         newDirection = "down";
-        newPos = isShiftPressed
-          ? { x: pos.x, y: pos.y }
-          : { x: pos.x, y: pos.y + 1 };
+        newPos = { x: pos.x, y: pos.y + 1 };
       } else if (keyPressed === "a") {
         newDirection = "left";
-        newPos = isShiftPressed
-          ? { x: pos.x, y: pos.y }
-          : { x: pos.x - 1, y: pos.y };
+        newPos = { x: pos.x - 1, y: pos.y };
       } else if (keyPressed === "d") {
         newDirection = "right";
-        newPos = isShiftPressed
-          ? { x: pos.x, y: pos.y }
-          : { x: pos.x + 1, y: pos.y };
+        newPos = { x: pos.x + 1, y: pos.y };
       }
 
       if (newDirection) {
         setDirection(newDirection);
         setWalking(true);
 
-        if (shouldNotCompleteMove(parsedMap.map, newPos)) {
+        if (shouldNotCompleteMove(parsedMap.map, newPos) || isShiftKeyPressed) {
           setWalking(false);
           return;
         }
