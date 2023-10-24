1be75e5fc0471b6adc58be00ab0dc9f75e099021
Henrique <rikezenho@gmail.com>
2023-10-24 09:19:43 -0300

    Migrate parcel to v2

--- src/components/map/tiles/Grass/index.js
+++ src/components/map/tiles/Grass/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { getRandomTile } from '../base';
-import VisualElement, { options } from './visual';
+import React from "react";
+import { getRandomTile } from "../base";
+import VisualElement, { options } from "./visual";
 
 const Grass = (props = {}) => {
-    const randomTile = getRandomTile('Grass', options);
-    return <VisualElement {...props} tile={randomTile} className='sqm' />;
+  const randomTile = getRandomTile("Grass", options);
+  return <VisualElement {...props} tile={randomTile} className="sqm" />;
 };
+
+Grass.metadata = {};
 
 export default Grass;
